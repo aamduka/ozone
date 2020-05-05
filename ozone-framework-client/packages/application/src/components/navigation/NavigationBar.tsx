3ab644573c186400bda9663eb737a66b0332c929
Akmal Amduka <akmal.amduka@nextcentury.com>
2020-05-05 18:44:51 -0400

    allow lock/unlock dashboard functionality for owner of dashboard or admin

--- ozone-framework-client/packages/application/src/components/navigation/NavigationBar.tsx
+++ ozone-framework-client/packages/application/src/components/navigation/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import styles from "./index.scss";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Response } from "../../api/interfaces";
 import { useBehavior } from "../../hooks";
 
 import { mainStore } from "../../stores/MainStore";
@@ -24,11 +25,40 @@ import {
     UserMenuButton,
     WidgetsButton
 } from "./internal";
+import { authStore } from '../../stores/AuthStore';
+import { DashboardDTO } from '../../api/models/DashboardDTO';
+import { dashboardApi } from '../../api/clients/DashboardAPI';
 
 const _NavigationBar: React.FC<PropsBase> = ({ className }) => {
     const isStoreOpen = useBehavior(mainStore.isStoreOpen);
     const dashboard = useBehavior(dashboardStore.currentDashboard);
     const { isLocked } = useBehavior(dashboard.state);
+    const currentUser = useBehavior(authStore.user);
+    const [isLockEnabled, setLockEnabled] = useState(false);
+
+    useEffect(() => {
+        isUserDashboardOwner().then((response: boolean) => {
+            setLockEnabled(response);
+        });    
+    });
+
+    const isUserDashboardOwner = async (): Promise<boolean> =>  {
+        const dashboardId = dashboard.state().value.id;
+        if (!dashboardId)
+            return false;
+
+        const response: Response<DashboardDTO> = await dashboardApi.getDashboard(dashboardId)
+        const stack =  response.data.stack;
+    
+        if (stack && stack.owner && currentUser)
+        {
+            return currentUser.isAdmin || currentUser.username == stack.owner.username;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     return (
         <Navbar className={classNames(styles.navbar, className)}>
@@ -45,7 +75,7 @@ const _NavigationBar: React.FC<PropsBase> = ({ className }) => {
             </NavbarGroup>
 
             <NavbarGroup className={styles.group} align={Alignment.RIGHT}>
-                <LockButton dashboard={dashboard} isLocked={isLocked} isStoreOpen={isStoreOpen} />
+                <LockButton dashboard={dashboard} isLocked={isLocked} isStoreOpen={isStoreOpen} isLockEnabled={isLockEnabled} />
                 <SaveDashboardButton isStoreOpen={isStoreOpen} />
                 <AddLayoutButton isLocked={isLocked} isStoreOpen={isStoreOpen} />
                 <NavbarDivider />
--- ozone-framework-client/packages/application/src/components/navigation/internal/LockButton.tsx
+++ ozone-framework-client/packages/application/src/components/navigation/internal/LockButton.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-import { useBehavior } from "../../../hooks";
 import { AnchorButton } from "@blueprintjs/core";
-
-import { dashboardStore } from "../../../stores/DashboardStore";
-import { mainStore } from "../../../stores/MainStore";
 
 import { Dashboard } from "../../../models/Dashboard";
 import { NavbarTooltip } from "./NavbarTooltip";
@@ -13,20 +9,21 @@ export interface LockButtonProps {
     dashboard: Dashboard;
     isStoreOpen: boolean;
     isLocked: boolean;
+    isLockEnabled: boolean;
 }
 
-const _LockButton: React.FC<LockButtonProps> = ({ dashboard, isLocked, isStoreOpen }) => {
+const _LockButton: React.FC<LockButtonProps> = ({ dashboard, isLocked, isStoreOpen, isLockEnabled }) => {
     const tooltipProps = {
         title: isLocked ? "Unlock Dashboard" : "Lock Dashboard",
-        description: isLocked
+        description: isLockEnabled ? (isLocked
             ? "Unlock the current Dashboard to allow modifications"
-            : "Lock the current Dashboard to prevent modifications"
+            : "Lock the current Dashboard to prevent modifications") : "Only owner can lock or unlock the dashboard"
     };
 
     const buttonProps = {
         icon: isLocked ? "lock" : "unlock",
         onClick: isLocked ? dashboard.unlock : dashboard.lock,
-        disabled: isStoreOpen
+        disabled: isStoreOpen || !isLockEnabled
     } as const;
 
     return (
